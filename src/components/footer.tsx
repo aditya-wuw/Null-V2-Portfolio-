a4eaa0eb37896ce01554036ed7523262dd827f63
JOhn_Mustard <166864125+aditya-wuw@users.noreply.github.com>
2026-01-01 18:53:32 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,14 @@ import { contactData } from '@/data/data'
 
 const Footer = () => {
   const { LightTheme } = createThemeContext()
-  const lastupdated = '28th Dec'
+  const lastupdated = '28th Dec 2025'
   return (
     <div
       className={`p-3 rounded-t-2xl text-center ${LightTheme ? 'bg-white text-black border border-black/30' : 'bg-black text-white border border-white/30'}`}
     >
       <h1 className="flex items-center justify-center gap-1 mb-1">
         <FaClock className={`${LightTheme ? 'text-black' : 'text-white'}`} />{' '}
-        {lastupdated + ' ' + new Date().getFullYear()}
+        {lastupdated}
         <div className="flex gap-1 ml-2">
           <a href={contactData.gh} target="_blank">
             <FaGithub className="w-4 h-4 cursor-pointer hover:scale-115 scale-100 transition-scale duration-300 ease-in-out" />
